19eb99d4cb8cf165aa2c3b227f690870ca2bcc2d
shipilov <shipilov2d90@gmail.com>
2020-05-04 22:04:35 +0300

    moving target aiming
    Useful if you trying to play with gamepad against pc gamers (no)

--- recent-scores.js
+++ recent-scores.js
@@ -1,25 +1,21 @@
 function RecentScores(username) {
 
-  this.scores = [];
-  this.counter = 0;
-  this.username = username;
+    this.scores = [];
+    this.counter = 0;
+    this.username = username;
 
-  this.display = function() {
-    for (var i = 0; i < this.scores.length && i < 5; i++) {
-      fill(255);
-      textSize(30);
-      text(username + ": " + this.scores[i], 50, 100+i*30);
+    this.display = function () {
+        for (var i = 0; i < this.scores.length && i < 5; i++) {
+            fill(255);
+            textSize(30);
+            text(username + ": " + this.scores[i], 50, 100 + i * 30);
+        }
     }
-  }
 
-  this.addScore = function(score) {
-    if (this.scores.length >= 5) {
-      this.scores.splice(this.scores.length-1);
+    this.addScore = function (score) {
+        if (this.scores.length >= 5) {
+            this.scores.splice(this.scores.length - 1);
+        }
+        this.scores.unshift(score);
     }
-    this.scores.unshift(score);
-  }
-
-
-
-
 }
--- sketch.js
+++ sketch.js
@@ -2,141 +2,67 @@ let aimTimer;
 let score;
 let numberOfStaticsHit = 0;
 let recentScores;
-let userClicks = [];
-let greeting, button, input;
+let greeting, button, input, select;
 let hasGameStarted = false;
 let numberOfClicksWhileGameStarted = 0;
-const NUMBER_OF_TARGETS = 5;
-
-let targets = [];
-
+let currentStrategy;
+const NUMBER_OF_TARGETS = 2;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(0);
+    createCanvas(windowWidth, windowHeight);
+    background(13);
 
+    input = createInput();
+    input.position(width / 2 - 250, height / 2);
 
-  input = createInput();
-  input.position(width/2-250, height/2);
+    button = createButton('Play');
+    button.position(width / 2 - 250, height / 2 + 80);
+    button.mousePressed(userHasInputName);
 
-  button = createButton('Play');
-  button.position(width/2-250, height/2+80);
-  button.mousePressed(userHasInputName);
+    select = createSelect();
+    select.position(width / 2 - 250, height / 2 + 150);
+    select.option("Static target firing", "static");
+    select.option("Moving target aiming", "moving");
 }
 
 function userHasInputName() {
-  button.style("visibility", "hidden");
-  input.style("visibility", "hidden");
-  hasGameStarted = true;
-  recentScores = new RecentScores(input.value());
+    button.style("visibility", "hidden");
+    input.style("visibility", "hidden");
+    select.style("visibility", "hidden");
+    recentScores = new RecentScores(input.value());
+    hasGameStarted = true;
 
-  restartGame();
+    initCurrentStrategy();
+    restartGame();
 }
 
 function restartGame() {
-  for (let i = 0; i < NUMBER_OF_TARGETS; i++) {
-    let hasDone = false
-    while(!hasDone) {
-      let newTarget = new Target(i, targets);
-      hasDone = newTarget.hasFoundTargetPosition(i);
-    }
-  }
-
-  aimTimer = new AimTimer();
-  score = new Score();
-
+    currentStrategy.run();
 }
-
 
 function draw() {
-  if (hasGameStarted) {
-    background(0);
-
-    aimTimer.display();
-    score.display();
-    recentScores.display();
-
-    for (let i = 0; i < NUMBER_OF_TARGETS; i++) {
-      if (targets[i].hasRespawned) {
-        userClicks.push(new MissedTarget(targets[i].previousPos.x, targets[i].previousPos.y, 3));
-        score.score -= 3;
-
-      }
-      targets[i].update();
-      targets[i].display();
-
+    if(hasGameStarted) {
+        currentStrategy.draw();
     }
-
-
-    if (frameCount % 60 == 0) {
-      aimTimer.time--;
-      if (aimTimer.time <= 0) {
-        recentScores.addScore(score.score);
-        restartGame();
-      }
-    }
-    displayUserClicks();
-
-  }
-
 }
 
-function displayUserClicks() {
-  for (let i = userClicks.length-1; i >= 0; i--) {
-    userClicks[i].update();
-    userClicks[i].display();
-    if (!userClicks[i].isVisible) {
-      userClicks.splice(i, 1);
-    }
-  }
-}
 function mousePressed() {
-  console.log(hasGameStarted);
-  if (hasGameStarted) {
-    numberOfClicksWhileGameStarted++;
-  }
-  if (numberOfClicksWhileGameStarted > 1) {
-    let hasHitStaticTarget = processUserClickedStaticTarget();
-    if (!hasHitStaticTarget) {
-      processedMissedTarget();
+    if(hasGameStarted) {
+        currentStrategy.mousePressed();
     }
-  }
 
 }
 
-
-function processUserClickedMovingTarget() {
-  let hasHitATarget = false;
-  let distance = dist(mouseX, mouseY, movingTarget.pos.x, movingTarget.pos.y);
-  if (distance < movingTarget.r) {
-    hasHitATarget = true;
-    score.score += 8;
-    userClicks.push(new HitTarget(mouseX, mouseY, 8));
-    movingTarget.beenClicked();
-  }
-
-  return hasHitATarget;
-}
-
-
-function processUserClickedStaticTarget() {
-  let hasHitATarget = false;
-  for (let i = 0; i < NUMBER_OF_TARGETS; i++) {
-    let distance = dist(mouseX, mouseY, targets[i].pos.x, targets[i].pos.y);
-    if (distance < targets[i].r) {
-      hasHitATarget = true;
-      console.log(targets[i].timeAlive);
-      userClicks.push(new HitTarget(mouseX, mouseY, Math.floor(600/targets[i].timeAlive)));
-      score.score+= Math.floor(600/targets[i].timeAlive);
-      targets[i].respawn(true);
-      numberOfStaticsHit++;
+function initCurrentStrategy() {
+    const item = select.value();
+    switch (item) {
+        case "static" : {
+            currentStrategy = new TargetAimStatic();
+        }
+        break;
+        case "moving": {
+            currentStrategy = new TargetAimMoving();
+        }
+        break;
     }
-  }
-  return hasHitATarget;
-}
-
-
-function processedMissedTarget() {
-  score.score -= 5;
-  userClicks.push(new MissedTarget(mouseX, mouseY, 5));
 }
